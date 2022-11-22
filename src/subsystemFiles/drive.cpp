c33c1e648053a22a428ab216516c5994d62690fe
SteveTheSteve1400 <you@example.com>
2022-11-21 22:15:08 -0500

    swapped drive

--- src/subsystemFiles/drive.cpp
+++ src/subsystemFiles/drive.cpp
@@ -11,8 +11,8 @@ void setDriveMotors(){
     //setDrive(0,0); --> all the drive motors are set to 0;
     //joystick --> analog and button --> digital
     //VOLTAGE OF MOTOR: + or - 127
-    int leftJoystick = -controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
-    int rightJoystick = -controller.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_Y);
+    int leftJoystick = controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
+    int rightJoystick = controller.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_Y);
     
     if(abs(leftJoystick) < 10){ //TO prevent from margin fo error of the stop position
         leftJoystick = 0;
